766f14714d3142fa47cb59f0d2b2f63d99f51c60
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-03-11 08:08:23 +0530

    all required libraries added, settings struct added

--- SENDER/SENDER.ino
+++ SENDER/SENDER.ino
@@ -1,7 +1,14 @@
 #include <Arduino.h>
-#include <math.h>
-#include <BH1750.h>
-#include <ArduinoJson.h>
+#include <math.h>            // To calculate NTC
+#include <Wire.h>            // I2C communication
+#include <SPI.h>             // For I2C communication
+#include <EEPROM.h>          // Settings storage
+#include <SPIFFS.h>          // File system
+#include <HardwareSerial.h>  // Serial communication
+#include <ArduinoJson.h>     // make json format
+#include <Preferences.h>     // To store users' settings
+#include <BH1750.h>          // For LUX measurement of sunlight
+
 
 #define RXD2 20
 #define TXD2 21
@@ -14,14 +21,14 @@
 // HardwareSerial rs485(1);
 
 // -- NTC Setup
-#define NTC_PIN 0              // ADC pin
+#define NTC_PIN 0               // ADC pin
 #define FIXED_RESISTOR 10000.0  // 10k fixed resistor
-#define R0 10000.0               // NTC resistance at 25°C
+#define R0 10000.0              // NTC resistance at 25°C
 #define BETA 3435.0             // 3950 (common value)
 #define T0 298.15               // 25°C in Kelvin
-#define OFFSET 0.0            // Adjust later (+ or -)
+#define OFFSET 0.0              // Adjust later (+ or -)
 #define ADC_RESOLUTION 4095.0   // 12 bits (1111 1111 1111)
-#define VREF 3.3               // Maximum sensing voltage of ESP32
+#define VREF 3.3                // Maximum sensing voltage of ESP32
 unsigned long lastNTCReadTime = 0;
 float ntcSampleSum = 0;
 uint8_t ntcSampleCount = 0;
@@ -42,6 +49,98 @@ uint8_t sunlightPercentage = 0;
 
 // -- Serial Output
 unsigned long lastSerialPrint = 0;
+
+// -- Global variables for settings
+Preferences preferences;
+struct DeviceSettings {
+  float ntcResistance;   // 10K in general
+  float betaConstant;    // 3435 in general
+  float ntcOffset;       // To fix error
+  uint16_t ntcInterval;  // Reading interval for NTC sensor
+
+  uint16_t luxInterval;  // Reading interval for BH1750 sensor
+
+  uint8_t enableRS485;      // 0 = disabled, 1 = enabled
+  uint8_t modbusDeviceID;   // 1–247
+  uint32_t modbusBaudRate;  // 9600, 19200, 38400 etc
+  uint16_t modbusInterval;  // seconds (share interval)
+};
+DeviceSettings settings;
+
+//////////////////////   DEFAULT SETTINGS   //////////////////////
+
+void setDefaultSettings() {
+  settings.ntcResistance = 10000.0;
+  settings.betaConstant = 3435.0;
+  settings.ntcOffset = 0.0;
+  settings.ntcInterval = 1;
+
+  settings.luxInterval = 1;
+
+  settings.enableRS485 = 0;
+  settings.modbusDeviceID = 1;
+  settings.modbusBaudRate = 115200;
+  settings.modbusInterval = 5;
+}
+
+//////////////////////   LOAD SETTINGS   //////////////////////
+
+void loadSettings() {
+
+  Serial.println("\n========== LOADING SETTINGS ==========");
+
+  preferences.begin("device", true);  // read-only
+
+  settings.ntcResistance = preferences.getFloat("ntcR", 10000.0);
+  settings.betaConstant = preferences.getFloat("beta", 3435.0);
+  settings.ntcOffset = preferences.getFloat("ntcOff", 0.0);
+  settings.ntcInterval = preferences.getUInt("ntcInt", 1);
+
+  settings.luxInterval = preferences.getUInt("luxInt", 1);
+
+  settings.enableRS485 = preferences.getUChar("rsEn", 0);
+  settings.modbusDeviceID = preferences.getUChar("rsID", 1);
+  settings.modbusBaudRate = preferences.getULong("rsBaud", 115200);
+  settings.modbusInterval = preferences.getUInt("rsInt", 5);
+
+  preferences.end();
+
+  // -------- PRINT LOADED SETTINGS --------
+  Serial.println("NTC Resistance        : " + String(settings.ntcResistance));
+  Serial.println("Beta Constant         : " + String(settings.betaConstant));
+  Serial.println("NTC Offset            : " + String(settings.ntcOffset));
+  Serial.println("NTC Interval (sec)    : " + String(settings.ntcInterval));
+
+  Serial.println("Lux Interval (sec)    : " + String(settings.luxInterval));
+
+  Serial.println("RS485 Enabled         : " + String(settings.enableRS485));
+  Serial.println("Modbus Device ID      : " + String(settings.modbusDeviceID));
+  Serial.println("Modbus Baud Rate      : " + String(settings.modbusBaudRate));
+  Serial.println("Modbus Interval (sec) : " + String(settings.modbusInterval));
+
+  Serial.println("=======================================\n");
+}
+
+//////////////////////   SAVE SETTINGS   //////////////////////
+
+void saveSettings() {
+  preferences.begin("device", false);  // write mode
+
+  preferences.putFloat("ntcR", settings.ntcResistance);
+  preferences.putFloat("beta", settings.betaConstant);
+  preferences.putFloat("ntcOff", settings.ntcOffset);
+  preferences.putUInt("ntcInt", settings.ntcInterval);
+
+  preferences.putUInt("luxInt", settings.luxInterval);
+
+  preferences.putUChar("rsEn", settings.enableRS485);
+  preferences.putUChar("rsID", settings.modbusDeviceID);
+  preferences.putULong("rsBaud", settings.modbusBaudRate);
+  preferences.putUInt("rsInt", settings.modbusInterval);
+
+  preferences.end();
+  Serial.println("Settings Saved to NVS");
+}
 
 // unsigned long sendInterval = 5000;
 // unsigned long lastSendTime = 0;
@@ -128,29 +227,25 @@ unsigned long lastSerialPrint = 0;
 //--------------------------------
 // Read averaged voltage
 //--------------------------------
-float readNTCVoltage()
-{
+float readNTCVoltage() {
   uint32_t sum = 0;
 
-  for (int i = 0; i < 20; i++)
-  {
+  for (int i = 0; i < 20; i++) {
     sum += analogReadMilliVolts(NTC_PIN);
   }
 
   float avgMilliVolt = sum / 20.0;
 
-  return avgMilliVolt / 1000.0;   // convert to volts
+  return avgMilliVolt / 1000.0;  // convert to volts
 }
 
 //--------------------------------
 // Calculate temperature
 //--------------------------------
-void calculateNTCFromADC()
-{
+void calculateNTCFromADC() {
   float voltage = readNTCVoltage();
 
-  if (voltage <= 0.001)
-  {
+  if (voltage <= 0.001) {
     ntcTemp = -100;
     return;
   }
@@ -167,16 +262,13 @@ void calculateNTCFromADC()
 //--------------------------------
 // Handle NTC every 1 second
 //--------------------------------
-void handleNTC()
-{
+void handleNTC() {
   unsigned long currentMillis = millis();
 
-  if (currentMillis - lastNTCReadTime >= 1000)
-  {
+  if (currentMillis - lastNTCReadTime >= 1000) {
     lastNTCReadTime = currentMillis;
 
     calculateNTCFromADC();
-
   }
 }
 
@@ -192,7 +284,6 @@ void handleLUX() {
 
   // ---- Read Sensor ----
   luxValue = lightMeter.readLightLevel();
-
 }
 
 //////////////////////   SERIAL OUTPUT   //////////////////////
@@ -240,8 +331,22 @@ void setup() {
 
   digitalWrite(LED_PIN, LOW);
 
-    // --- LUX Sensor Init ---
-  // u8g2.clearBuffer();
+  // --- Storage Init ---
+  Serial.println("\n==============================");
+  Serial.println("Settings Initialization");
+  Serial.println("==============================");
+  Serial.println("[INFO] Initializing EEPROM...");
+  delay(1000);
+
+  EEPROM.begin(512);
+  loadSettings();
+
+  Serial.println("[SUCCESS] EEPROM Initialized.");
+  Serial.printf("[INFO] EEPROM Size: %d bytes\n", 512);
+  Serial.println("==============================\n");
+  delay(1000);
+
+  // --- LUX Sensor Init ---
   Serial.println("\n==============================");
   Serial.println("BH1750 Initialization");
   Serial.println("==============================");
@@ -249,17 +354,14 @@ void setup() {
 
   Wire.begin();  // SDA, SCL default for ESP32
   if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
-    // u8g2.drawStr(0, 18, "BH1750: OK");
     Serial.println("[SUCCESS] BH1750 detected.");
     Serial.println("[INFO] Mode: Continuous High Resolution");
     Serial.println("==============================\n");
   } else {
-    // u8g2.drawStr(0, 18, "BH1750: ERROR");
     Serial.println("[ERROR] BH1750 not detected!");
     Serial.println("[INFO] Check SDA/SCL wiring.");
     Serial.println("==============================\n");
   }
-  // u8g2.sendBuffer();
   delay(1000);
 
 
@@ -270,9 +372,9 @@ void setup() {
 
 void loop() {
 
-handleNTC();
-handleLUX();
-serialOutput();
+  handleNTC();
+  handleLUX();
+  serialOutput();
 
   // unsigned long currentMillis = millis();
 
